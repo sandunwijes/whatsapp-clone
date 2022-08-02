eea425559678f72916c45a85bcdbc8001beaead8
sandunwijes <107408557+sandunwijes@users.noreply.github.com>
2022-08-02 13:11:25 +0530

    add major components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Chat from './components/Chat';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      
+      <div className="app__body">
+
+        <h1>body</h1>
+        <Sidebar/>
+        <Chat/>
+      </div>
     </div>
   );
 }
